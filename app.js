c219e53c14fac9bde82a1dc082d087c885b21bf7
Michael Osei <mosei27@live.com>
2017-09-30 12:22:07 -0700

    Add server init scripts

---
+++ app.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+var index = require('./routes/index');
+var path = require('path');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// set up routing
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(8000, function () {
+  console.log('listening on port 8000!')
+});
+
+module.exports = app;
