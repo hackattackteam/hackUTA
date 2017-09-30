f02bc95ffc0ae4d4bc0fa78432671b0955b794b9
Michael Osei <mosei27@live.com>
2017-09-30 12:56:27 -0700

    Add webpack and babel configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,24 @@
-webpack.config.js
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'public/build');
+var APP_DIR = path.resolve(__dirname, 'public/app');
+
+var config = {
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module : {
+    loaders : [
+      {
+        test : /\.jsx?/,
+        include : APP_DIR,
+        loader : 'babel-loader'
+      }
+    ]
+  }
+};
+
+module.exports = config;
